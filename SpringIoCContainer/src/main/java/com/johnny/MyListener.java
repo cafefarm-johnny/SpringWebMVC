3875ebaff9cb49f0dd799bdddfddda6b8af18834
cafefam-johnny <npcdja@gmail.com>
2019-02-04 19:28:45 +0900

    스프링 웹 MVC - 1부. 스프링 MVC 동작 원리 - 스프링 IoC 컨테이너 연동

---
+++ SpringIoCContainer/src/main/java/com/johnny/MyListener.java
@@ -0,0 +1,18 @@
+package com.johnny;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("Servlet Listener contextInit!!!");
+		sce.getServletContext().setAttribute("name", "Johnny");
+	}
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("Servlet Listener contextDestroy!!!");
+	}
+}
